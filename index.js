545149b21ec077a0486e879bc76e2c02194f495f
Kamau-sam <skamau.6411@gmail.com>
2024-03-17 10:37:30 +0300

    completed test

---
+++ index.js
@@ -0,0 +1,40 @@
+function shout(string){
+return "Hello".toLocaleUpperCase();
+ }
+    
+function whisper(string){
+return "HELLO".toLowerCase();
+}
+
+function logShout(string){
+console.log("hello".toLocaleUpperCase());
+} logShout(`HELLO`);
+    
+function logWhisper(string){
+    console.log(string.toLowerCase());
+} logWhisper(`HELLO`);
+    
+function sayHiToHeadphonedRoommate(string){
+if(string===string.toLowerCase())
+return "I can't hear you!";
+}
+    
+function sayHiToHeadphonedRoommate(string){
+      if (string === string.toLowerCase()) {
+return "I can't hear you!";}
+      else if (string === string.toUpperCase()) {
+return "YES INDEED!";}
+}
+    
+ 
+function sayHiToHeadphonedRoommate(string){
+    if (string.toLowerCase()===string){
+return "I can't hear you!"}
+    if (string.toUpperCase()===string){
+return "YES INDEED!"}
+        if (string==="Let's have dinner together!"){
+return "I would love to!"
+
+}
+}
+    
